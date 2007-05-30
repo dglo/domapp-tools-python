deedfff1a4629d42adc154ffb02a2cf5fd6b5648
John Jacobsen <john@mail.npxdesigns.com>
2007-05-30 12:56:08 -0500

    Issue 1485: Use distutils to package up python part of domapp-tools

---
+++ domapptools/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# __init__.py
+# John Jacobsen, NPX Designs, Inc., jacobsen\@npxdesigns.com
+# Started: Wed May 30 16:54:46 2007
+
+def main():
+    pass
+
+if __name__ == "__main__": main()
+
